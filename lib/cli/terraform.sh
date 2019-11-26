5ceefeabd09d25d6a55a46b564050fb91e54abab
Nathan Marley <nathan.marley@gmail.com>
2019-11-26 11:54:04 -0300

    More reductions

--- lib/cli/terraform.sh
+++ lib/cli/terraform.sh
@@ -14,43 +14,27 @@ function terraform_init() {
 }
 
 function terraform_select_workspace() {
-    set +e
-
-    terraform workspace select ${1} 2>/dev/null
-
-    local exit_code="$?"
-
-    set -e
-
-    if [ ${exit_code} != 0 ]; then
-        terraform workspace new ${1}
-    fi
+    terraform workspace select $1
 }
 
 function terraform_run_command() {
     cd terraform/aws
-
     terraform_init
 
     # Select/create terraform workspace according to dash network name
-
     terraform_select_workspace "$NETWORK_NAME"
 
     # Setup the AWS infrastructure with terraform
-
     local config_path_arg=""
     if [ -f "../../$TERRAFORM_CONFIG_PATH" ]; then
         config_path_arg="-var-file=../../$TERRAFORM_CONFIG_PATH";
     fi
 
-    terraform ${1} -var "public_key_path=$PUBLIC_KEY_PATH" \
+    terraform $1 -var "public_key_path=$PUBLIC_KEY_PATH" \
                     ${config_path_arg}
 
-
     # Create the inventory file for Ansible
-
     terraform output ansible_inventory > ../../${INVENTORY_FILE}
-
     cd ../../
 }
 
@@ -59,15 +43,11 @@ function terraform_output_inventory() {
         echo "Ansible inventory '$INVENTORY_FILE' file not found. Trying to retrieve..."
 
         cd terraform/aws
-
         terraform_init
-
         terraform workspace select "$NETWORK_NAME"
-
         terraform output ansible_inventory > ../../${INVENTORY_FILE}
 
         echo "Ansible inventory file successfully created."
-
         cd ../../
     fi
 }
